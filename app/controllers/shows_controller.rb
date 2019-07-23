8919635f7cdd5798b7df0fd4099a45a9b2bc24d1
Sam Marshall <samuel.r.marshall@gmail.com>
2019-07-22 21:07:27 -0400

    almost there

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,8 +1,27 @@
 class ShowsController < ApplicationController
 
+  def index
+    if param[:showdate]
+      @shows = Show.where(showdate: params[:showdate])
+    elsif param[:venue]
+      @shows = Show.where(venue: params[:venue])
+    else
+      @shows = Show.all
+    end
+
+  end
+
   def show
     @show = Show.find(params[:id])
     render json: @show, status: 200
+  end
+
+  def by_showdate
+    render json: @shows, status: 200
+  end
+
+  def show_params
+    params.require(:show).permit(:link, :location, :notes, :showdate, :show_id, :tour_when, :tour_id, :tour_name, :venue, :venue_id, :rating, :setlist, :songs, :sets)
   end
 
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,7 @@
 class Show < ApplicationRecord
+
+  def self.lookup_by_date(date)
+    self.where(showdate: date).ids
+  end
+
 end
