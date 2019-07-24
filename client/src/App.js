b3d342c414b3faebbf4ae5897947770ce1707ff7
Sam (Personal) <samuel.r.marshall@gmail.com>
2019-07-24 18:50:51 -0400

    changed App to component, added state to props

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ShowContainer from './containers/ShowContainer'
+import {fetchShow} from './actions/showActions'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Phrank!</h1>
-      <h2>The application for tracking, ranking, and listening to your phavorite Phish shows.</h2>
-      <ShowContainer />
-    </div>
-  );
+class App extends Component {
+  componentDidMount(){
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <h1>Phrank!</h1>
+        <h2>The application for tracking, ranking, and listening to your phavorite Phish shows.</h2>
+      <ShowContainer fetchShow={this.props.fetchShow}/>
+    </div>)
+  };
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {shows: state.shows}
+}
+
+export default connect(mapStateToProps, {fetchShow})(App);
