875a4982d9816d808c4bd961a37662054138ceaf
Sam (Personal) <samuel.r.marshall@gmail.com>
2019-07-24 18:22:24 -0400

    added reducers

---
+++ client/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+import showReducer from './showReducer';
+
+const rootReducer = combineReducers({ shows: showReducer });
+export default rootReducer;
--- client/src/reducers/showReducer.js
+++ client/src/reducers/showReducer.js
@@ -1,11 +1,13 @@
 export default function showReducer(state= {
-
+  shows: [],
+  loading: false
 }, action){
   switch (action.type) {
-    case "test":
-      return "it worked"
-
+    case "LOADING_SHOW":
+      return {...state, loading: true};
+    case "ADD_SHOW":
+      return {...state, loading: false, shows: [...state.shows, action.payload]}
     default:
-        return state;
+      return state;
   }
 }
