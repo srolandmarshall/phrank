6ad65ca5adeb8d56f8aa336371e6a1bc8dbd6bb7
Sam (Personal) <samuel.r.marshall@gmail.com>
2019-07-25 11:20:11 -0400

    added Fetch to handleSubmit

--- client/src/actions/showActions.js
+++ client/src/actions/showActions.js
@@ -3,8 +3,10 @@ import fetch from 'isomorphic-fetch';
 export const fetchShow = (showDate) => {
  return (dispatch) => {
    dispatch({type:"LOADING_SHOW"});
-   return fetch('http://localhost:3001/shows/?='+showDate)
+   const url = 'http://localhost:3001/shows/?showdate='+showDate
+   console.log(url);
+   return fetch(url)
      .then(response=>response.json())
-     .then(data=>dispatch({type: "ADD_SHOW", payload: data}))
+     .then(data=>dispatch({type:"ADD_SHOW", payload: data}))
  }
 }
