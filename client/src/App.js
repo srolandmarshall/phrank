82b0180d94bfb7dcd68bda1a22c910f74c4e16f7
Sam Marshall <samuel.r.marshall@gmail.com>
2019-08-01 23:58:30 -0400

    added navbar from bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ShowContainer from './containers/ShowContainer'
 import {fetchShow} from './actions/showActions'
+import { Navbar, Nav } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
 
 class App extends Component {
@@ -11,6 +13,13 @@ class App extends Component {
   render() {
     return(
       <div className="App">
+        <Navbar bg="dark" variant="dark" sticky="top">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/addshow">Add Show</Nav.Link>
+            <Nav.Link href="/reviews">Reviews</Nav.Link>
+          </Nav>
+        </Navbar>
         <h1>Phrank!</h1>
         <h2>The application for tracking, ranking, and listening to your phavorite Phish shows.</h2>
         <ShowContainer shows={this.props.shows}       deleteShow={this.props.deleteShow}
