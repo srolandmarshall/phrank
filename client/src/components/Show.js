06f1423c9566369b98fa904334907fb5b800b1cd
Sam (Personal) <samuel.r.marshall@gmail.com>
2019-10-09 22:00:58 -0400

    updated show components, review deleted when show is deleted

--- client/src/components/Show.js
+++ client/src/components/Show.js
@@ -2,11 +2,19 @@ import React, {Component} from 'react';
 import parse from 'html-react-parser';
 import ReviewsContainer from '../containers/ReviewsContainer'
 import { Button } from 'react-bootstrap'
+import { removeUserShow } from '../actions/showActions'
+import { deleteReview } from '../actions/reviewActions'
+import { connect } from 'react-redux'
+
 
 class Show extends Component {
 
   handleClick = () => {
-    this.props.removeUserShow(this.props.show.id, this.props.user)
+    const showId = this.props.show.id
+    const user = this.props.user
+    const review = this.props.reviews.reviews.find(function(obj){ return obj.show_id === showId});
+    this.props.removeUserShow(showId, user)
+    this.props.deleteReview(review, user)
   }
 
   showReviews = (reviews, show) => {
@@ -18,14 +26,18 @@ class Show extends Component {
     return (
       <div className="show">
         <h4>{show.showdate}</h4>
-        <h4>{show.venue + ", " + show.location}
-          <Button variant="outline-danger" size="sm" onClick={()=>this.handleClick()}>Remove Show</Button>
-        </h4>
+        <h5>{show.venue + ", " + show.location}</h5>
         <div>{parse(show.setlist)}</div>
+        <Button variant="outline-danger" size="sm" onClick={()=>this.handleClick()}>Remove Show</Button>
         <ReviewsContainer user={this.props.user} show={show} reviews={this.showReviews(this.props.reviews.reviews, show)}/>
       </div>)
   }
 
 }
 
-export default Show;
+const mapDispatchToProps = dispatch => ({
+  removeUserShow: (user, show, review) => dispatch(removeUserShow(user, show, review)),
+  deleteReview: (review, user) => dispatch(deleteReview(review, user))
+})
+
+export default connect(null,mapDispatchToProps)(Show);
