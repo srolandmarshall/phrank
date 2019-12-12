49974ba0c43a8419705868ae13ad01099d10869b
Sam (Personal) <samuel.r.marshall@gmail.com>
2019-12-11 22:55:32 -0500

    Transformed myShows into functional component

--- client/src/components/myShows.js
+++ client/src/components/myShows.js
@@ -2,15 +2,12 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import MyShowsContainer from '../containers/MyShowsContainer'
 
-class MyShows extends React.Component{
-
-  render(){
+function MyShows(props){
   return (
     <Container>
       <h3>My Shows</h3>
       <MyShowsContainer />
     </Container>
   )}
-}
 
 export default MyShows
